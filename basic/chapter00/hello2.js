0416f00b1c2f4942b57d834671b1cab801087148
Lee YoungCheol <ofetwo@gmail.com>
2020-07-16 00:31:58 +0900

    Upload basic chapter01, 02

--- basic/chapter00/hello2.js
+++ basic/chapter00/hello2.js
@@ -1,2 +1,2 @@
 // This is my first Javascript code!
-console.log("Hello, World!");
+console.log("Hello, World! Again!");
---
+++ basic/chapter00/node.js
@@ -0,0 +1,6 @@
+// 실행 : 터미널에서
+// % node node.js
+console.log("Good-bye, World!");
+
+// 당연히 아래도 가능하다.
+// % node hello2.js
